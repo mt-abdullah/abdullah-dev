fa36ffc74ff840989a93f0141a623d8d77443fd4
mt-abdullah <iammtabdullah@gmail.com>
2025-08-27 13:46:19 +0530

    Updates

--- assets/index.es-9RyMcIP1.js
+++ assets/index.es-9RyMcIP1.js
@@ -1,4 +1,4 @@
-import{g as bn,R as Ne}from"./index-ClI9Zv0K.js";/*!
+import{g as bn,R as Ne}from"./index-3q3wHKM3.js";/*!
  * Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
